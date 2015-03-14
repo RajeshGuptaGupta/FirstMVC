63eb980a4bdea6fd9d4d64450935c55eac79c195
rajeshgupta <rajeshguptag008@gmail.com>
2015-03-15 01:50:08 +0530

    Second Commit

--- MvcApplication1/MvcApplication1/Global.asax.cs
+++ MvcApplication1/MvcApplication1/Global.asax.cs
@@ -18,10 +18,7 @@ namespace MvcApplication1
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
             AuthConfig.RegisterAuth();
         }
     }
